ffcfb6dc4d420b06a17f585a925ce6c9dffb3fd1
SamuelOchieng <o.samuelyannick@gmail.com>
2025-03-05 13:02:50 +0300

    Commit

---
+++ app/migrations/versions/e60b41bed465_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: e60b41bed465
+Revises: 
+Create Date: 2025-03-05 12:41:28.259661
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e60b41bed465'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
